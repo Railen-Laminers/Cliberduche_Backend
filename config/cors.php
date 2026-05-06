2dd14ff0bfb7fb160dfb4ea064644beacd170437
Railen-Laminers <railen.laminero@gmail.com>
2026-05-06 16:49:24 +0800

    update cors2

--- config/cors.php
+++ config/cors.php
@@ -9,7 +9,7 @@ return [
     'allowed_origins' => [
         'http://localhost:3000',
         'http://localhost:5173',
-        'https://cliberduchewebsite.vercel.app',
+        'https://cliberduche-frontend.vercel.app',
     ],
 
     'allowed_origins_patterns' => [],
